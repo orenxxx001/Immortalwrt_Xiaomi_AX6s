a8767ea136dfdeae423703ff291cba359a1d091e
orenxxx001 <33869335+orenxxx001@users.noreply.github.com>
2022-03-30 02:51:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,19 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.88.8/g' package/base-files/files/bin/config_generate
+
+sed -i 's/root::0:0:99999:7:::/root:$1$MhPcOOTE$DOOyDUwKjP9xnoSfaczsk.:19058:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# 修改hostname
+sed -i 's/OpenWrt/XinV-2.0/g' package/base-files/files/bin/config_generate
+
+# 添加主题
+git clone darkmatter https://github.com/apollo-ng/luci-theme-darkmatter.git package/luci-theme-darkmatter
+git clone neobird https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+
+# 替换默认主题为 luci-theme-darkmatter
+sed -i 's/luci-theme-argon/luci-theme-darkmatter/g' feeds/luci/collections/luci/Makefile
 
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
